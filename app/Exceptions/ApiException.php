cff6d94f1b2317456942a16e8de62bbd5e88b2b7
leptyagin <leptyagin.ser@gmail.com>
2026-09-18 22:18:12 +0700

    feat: exception for feed

--- app/Exceptions/ApiException.php
+++ app/Exceptions/ApiException.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\Exceptions;
 
-use DomainException;
+use Exception;
 
-abstract class ApiException extends DomainException
+abstract class ApiException extends Exception
 {
     public function __construct(string $message, public readonly int $statusCode)
     {
